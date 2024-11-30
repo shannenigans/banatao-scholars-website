3992423df32e770c732f8cb2b5fdd0c2183032a1
Benedict Taguinod <benedict.a.taguinod@gmail.com>
2024-11-29 18:58:54 -0800

    create basic landing page and profile cards

---
+++ src/types/scholar.ts
@@ -0,0 +1,10 @@
+export interface Scholar {
+  id: string;
+  name: string;
+  location: string;
+  description: string;
+  school: string;
+  year_graduated: number;
+  company: string;
+  imageUrl: string;
+}
