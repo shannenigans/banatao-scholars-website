d5ca3318713e03828178787565f1448195a1ce04
Shannen Barrameda <sbarrameda17@gmail.com>
2024-12-02 23:16:45 -0800

    setup supabase, boilerplate, initial sign in

---
+++ global.d.ts
@@ -0,0 +1,1 @@
+declare function handleSignInWithGoogle(): void;
---
+++ sign-in-utils.ts
@@ -0,0 +1,10 @@
+
+async function handleSignInWithGoogle(response) {
+    const { data, error } = await supabase.auth.signInWithIdToken({
+      provider: 'google',
+      token: response.credential,
+    })
+
+    console.log(data);
+  }
+  
